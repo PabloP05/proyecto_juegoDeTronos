db0ef406f6904c0500b13118118debe64103c6b8
PabloP05 <pablopardorodriguez.guadalupe@alumnado.fundacionloyola.net>
2025-10-31 12:01:55 +0100

    clase arma creada

--- Class/arma.js
+++ Class/arma.js
@@ -1,5 +1,35 @@
 export class Arma{
     #nombre;
     #daño;
-    #tipo
+    #tipo;
+
+    constructor(daño = 0,tipo= '',nombre= '') {
+        this.#daño = daño;
+        this.#tipo = tipo;
+        this.#nombre = nombre;
+    }
+
+    set nombre(nombre) {
+        this.#nombre = nombre;
+    }
+    set daño(daño) {
+        this.#daño = daño;
+    }
+    set tipo(tipo) {
+        this.#tipo = tipo;
+    }
+
+    get obtenerNombre() {
+        return this.#nombre;
+    }
+    get obtenerDaño() {
+        return this.#daño;
+    }
+    get obtenerTipo() {
+        return this.#tipo;
+    } 
+
+    descripcion() {
+        console.log(`${this.#nombre} tiene un daño de ${this.#daño} y es del tipo ${this.#tipo}`);
+    }
 }
